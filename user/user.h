302a54ef6bc8956738fbb878a32b6c78be1c824e
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-23 14:47:23 -0500

    Declare clone() and join() syscalls for users

--- user/user.h
+++ user/user.h
@@ -25,6 +25,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int clone(void (*start_routine)(void*), void *, void *);
+int join(void**);
 
 // user library functions (ulib.c)
 int stat(char*, struct stat*);
