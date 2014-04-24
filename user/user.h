89902a4dd8a0f899743034484e4346f569a4e72c
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-24 11:12:02 -0500

    reorder thread methods in header

--- user/user.h
+++ user/user.h
@@ -45,11 +45,11 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
-int thread_create(void (*start_routine)(void*), void *);
-int thread_join();
 void lock_init(lock_t *);
 void lock_acquire(lock_t *); 
 void lock_release(lock_t *);
+int thread_create(void (*start_routine)(void*), void *);
+int thread_join();
 
 #endif // _USER_H_
 
