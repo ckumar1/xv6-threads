3f8b318094d0842a0bd72674f5e6b19d913024c7
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-24 10:42:08 -0500

    Starts removing CV syscall and user function references

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -134,7 +134,7 @@ int sys_sleepcv(void) {
     return -1;
   lock_t *lock = (lock_t *)temp;
 
-  sleepcv(cv, lock);
+  sleep2(cv, lock);
 
   return 0;
 }
@@ -145,7 +145,7 @@ int sys_wakecv(void) {
   if(argptr(0, (void *)&cv, sizeof(void *)) < 0)
     return -1;
 
-  wakeupcv(cv);
+  wakeup2(cv);
 
   return 0;
 }
